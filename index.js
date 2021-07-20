b68498bb68fbf5e103d604cae8e14bc69258e6d1
Simon Siefke <simon.siefke@gmail.com>
2021-07-20 09:58:25 +0200

    add id to dt elements

--- index.js
+++ index.js
@@ -1,113 +1,115 @@
-import { parseCsp } from './parseCsp.js'
+import { parseCsp } from "./parseCsp.js";
 
-const $Url = document.getElementById('Url')
-const $Form = document.getElementById('Form')
-let $Output = document.getElementById('Output')
+const $Url = document.getElementById("Url");
+const $Form = document.getElementById("Form");
+let $Output = document.getElementById("Output");
 
-const compress = (value) => LZString.compressToBase64(value)
+const compress = (value) => LZString.compressToBase64(value);
 
-const decompress = (value) => LZString.decompressFromBase64(value)
+const decompress = (value) => LZString.decompressFromBase64(value);
 
 const updateUrl = () => {
-  const value = $Url.value
+  const value = $Url.value;
   const newUrl =
     window.location.protocol +
-    '//' +
+    "//" +
     window.location.host +
     window.location.pathname +
-    '?url=' +
-    encodeURIComponent(value)
-  history.pushState({ path: newUrl }, '', newUrl)
-}
+    "?url=" +
+    encodeURIComponent(value);
+  history.pushState({ path: newUrl }, "", newUrl);
+};
 
-let pending = false
+let pending = false;
 
 const updateUrlThrottled = () => {
   if (pending) {
-    return
+    return;
   }
-  pending = true
-  updateUrl()
+  pending = true;
+  updateUrl();
   setTimeout(() => {
-    updateUrl()
-    pending = false
-  }, 30)
-}
+    updateUrl();
+    pending = false;
+  }, 30);
+};
 
 const handleChange = async (event) => {
-  const $NewOutput = document.createElement('small')
-  $NewOutput.textContent = '(loading)'
-  $Output.replaceWith($NewOutput)
-  $Output = $NewOutput
+  const $NewOutput = document.createElement("small");
+  $NewOutput.textContent = "(loading)";
+  $Output.replaceWith($NewOutput);
+  $Output = $NewOutput;
   if (event) {
-    event.preventDefault()
+    event.preventDefault();
   }
-  const url = $Url.value
+  const url = $Url.value;
   if (!url) {
-    return
+    return;
   }
-  let csp = ''
+  let csp = "";
   try {
-    const res = await fetch(`https://cors.bridged.cc/${url}`)
+    const res = await fetch(`https://cors.bridged.cc/${url}`);
     if (res.status === 404) {
-      const $NewOutput = document.createElement('small')
-      $NewOutput.textContent = '(not found)'
-      $Output.replaceWith($NewOutput)
-      $Output = $NewOutput
-      return
+      const $NewOutput = document.createElement("small");
+      $NewOutput.textContent = "(not found)";
+      $Output.replaceWith($NewOutput);
+      $Output = $NewOutput;
+      return;
     }
-    const headers = res.headers
-    csp = headers.get('Content-Security-Policy') || ''
+    const headers = res.headers;
+    csp = headers.get("Content-Security-Policy") || "";
   } catch (error) {
-    const $NewOutput = document.createElement('small')
-    $NewOutput.textContent = `(error ${error})`
-    $Output.replaceWith($NewOutput)
-    $Output = $NewOutput
-    return
+    const $NewOutput = document.createElement("small");
+    $NewOutput.textContent = `(error ${error})`;
+    $Output.replaceWith($NewOutput);
+    $Output = $NewOutput;
+    return;
   }
   if (!csp) {
-    const $NewOutput = document.createElement('small')
-    $NewOutput.textContent = '(empty csp)'
-    $Output.replaceWith($NewOutput)
-    $Output = $NewOutput
-    return
+    const $NewOutput = document.createElement("small");
+    $NewOutput.textContent = "(empty csp)";
+    $Output.replaceWith($NewOutput);
+    $Output = $NewOutput;
+    return;
   }
-  const parsedCsp = parseCsp(csp)
+  const parsedCsp = parseCsp(csp);
   if (parsedCsp.errors.length > 0) {
-    const $NewOutput = document.createElement('pre')
-    $NewOutput.textContent = JSON.stringify(parsedCsp.errors, null, 1)
-    $Output.replaceWith($NewOutput)
-    $Output = $NewOutput
+    const $NewOutput = document.createElement("pre");
+    $NewOutput.textContent = JSON.stringify(parsedCsp.errors, null, 1);
+    $Output.replaceWith($NewOutput);
+    $Output = $NewOutput;
   } else {
-    let $Dl = document.createElement('dl')
+    let $Dl = document.createElement("dl");
     for (const [key, values] of Object.entries(parsedCsp.result)) {
-      const $Dt = document.createElement('dt')
-      $Dt.textContent = key
-      $Dl.append($Dt)
-      const sortedValues = values.slice().sort((a, b) => a.localeCompare(b))
+      const $Dt = document.createElement("dt");
+      $Dt.textContent = key;
+      $Dt.id = key;
+      $Dl.append($Dt);
+
+      const sortedValues = values.slice().sort((a, b) => a.localeCompare(b));
       for (const value of sortedValues) {
-        const $Dd = document.createElement('dd')
-        $Dd.textContent = value
-        $Dl.append($Dd)
+        const $Dd = document.createElement("dd");
+        $Dd.textContent = value;
+        $Dl.append($Dd);
       }
     }
 
-    $Output.replaceWith($Dl)
-    $Output = $Dl
+    $Output.replaceWith($Dl);
+    $Output = $Dl;
   }
 
-  updateUrlThrottled()
-}
+  updateUrlThrottled();
+};
 
-if ('URLSearchParams' in window) {
-  const searchParams = new URLSearchParams(window.location.search)
-  const url = searchParams.get('url')
+if ("URLSearchParams" in window) {
+  const searchParams = new URLSearchParams(window.location.search);
+  const url = searchParams.get("url");
   if (url) {
-    const decoded = decodeURIComponent(url)
-    $Url.value = decoded
-    handleChange()
+    const decoded = decodeURIComponent(url);
+    $Url.value = decoded;
+    handleChange();
   }
 }
 
-$Form.onsubmit = handleChange
+$Form.onsubmit = handleChange;
 // $Url.oninput = handleChange
--- parseCsp.js
+++ parseCsp.js
@@ -1,143 +1,143 @@
 const validValues = new Set([
-  'default-src',
-  'style-src',
-  'script-src',
-  'frame-src',
-  'frame-ancestors',
-  'font-src',
-  'connect-src',
-  'media-src',
-  'object-src',
-  'img-src',
-  'base-uri',
-  'block-all-mixed-content',
-  'upgrade-insecure-requests',
-  'report-uri',
-])
+  "default-src",
+  "style-src",
+  "script-src",
+  "frame-src",
+  "frame-ancestors",
+  "font-src",
+  "connect-src",
+  "media-src",
+  "object-src",
+  "img-src",
+  "base-uri",
+  "block-all-mixed-content",
+  "upgrade-insecure-requests",
+  "report-uri",
+]);
 
-const RE_KEY = /^[a-zA-Z\d\-]+/
-const RE_WHITESPACE = /^\s+/
-const RE_VALUE = /^[^\s;]+/
-const RE_SEMICOLON = /^;/
+const RE_KEY = /^[a-zA-Z\d\-]+/;
+const RE_WHITESPACE = /^\s+/;
+const RE_VALUE = /^[^\s;]+/;
+const RE_SEMICOLON = /^;/;
 
 export const parseCsp = (csp) => {
-  if (csp.startsWith('Content-Security-Policy:')) {
-    csp = csp.slice('Content-Security-Policy:'.length)
+  if (csp.startsWith("Content-Security-Policy:")) {
+    csp = csp.slice("Content-Security-Policy:".length);
   }
-  let state = 'top'
-  let next = null
-  const result = Object.create(null)
-  const warnings = []
-  const errors = []
-  let index = 0
-  let key = ''
+  let state = "top";
+  let next = null;
+  const result = Object.create(null);
+  const warnings = [];
+  const errors = [];
+  let index = 0;
+  let key = "";
   outer: while (index < csp.length) {
     switch (state) {
-      case 'top':
+      case "top":
         if ((next = csp.slice(index).match(RE_WHITESPACE))) {
-          const text = next[0]
-          state = 'top'
-          index += text.length
-          break
+          const text = next[0];
+          state = "top";
+          index += text.length;
+          break;
         }
         if ((next = csp.slice(index).match(RE_KEY))) {
-          const text = next[0]
+          const text = next[0];
           if (result[text]) {
-            warnings.push({ message: `duplicate key ${text}` })
+            warnings.push({ message: `duplicate key ${text}` });
           } else {
-            result[text] = []
+            result[text] = [];
           }
-          state = 'after-key'
-          index += text.length
-          key = text
-          break
+          state = "after-key";
+          index += text.length;
+          key = text;
+          break;
         }
         if ((next = csp.slice(index).match(RE_SEMICOLON))) {
-          const text = next[0]
+          const text = next[0];
           warnings.push({
-            message: 'unnecessary semicolon',
+            message: "unnecessary semicolon",
             index,
             state,
             text: csp.slice(index, index + 15),
-          })
-          state = 'top'
-          index += text.length
-          break
+          });
+          state = "top";
+          index += text.length;
+          break;
         }
         errors.push({
-          message: 'parsing error',
+          message: "parsing error",
           index,
           state,
           text: csp.slice(index, index + 15),
-        })
-        console.log('break out')
-        break outer
-      case 'after-key':
+        });
+        console.log("break out");
+        break outer;
+      case "after-key":
         if ((next = csp.slice(index).match(RE_SEMICOLON))) {
-          const text = next[0]
-          state = 'top'
-          index += text.length
-          break
+          const text = next[0];
+          state = "top";
+          index += text.length;
+          break;
         }
         if ((next = csp.slice(index).match(RE_WHITESPACE))) {
-          const text = next[0]
-          state = 'after-whitespace'
-          index += text.length
-          break
+          const text = next[0];
+          state = "after-whitespace";
+          index += text.length;
+          break;
         }
-        index
-        console.log('parse error')
-        csp.slice(index) //?
+        index;
+        console.log("parse error");
+        csp.slice(index); //?
         errors.push({
           message: `parsing error`,
           index,
           state,
           text: csp.slice(index, index + 15),
-        })
-        break outer
-      case 'after-whitespace':
+        });
+        break outer;
+      case "after-whitespace":
         if ((next = csp.slice(index).match(RE_VALUE))) {
-          const text = next[0]
-          state = 'after-value'
-          index += text.length
-          result[key].push(text)
-          break
+          const text = next[0];
+          state = "after-value";
+          index += text.length;
+          result[key].push(text);
+          break;
         }
         errors.push({
           message: `parsing error`,
           index,
           state,
           text: csp.slice(index, index + 15),
-        })
-        break outer
-      case 'after-value':
+        });
+        break outer;
+      case "after-value":
         if ((next = csp.slice(index).match(RE_WHITESPACE))) {
-          const text = next[0]
-          state = 'after-whitespace'
-          index += text.length
+          const text = next[0];
+          state = "after-whitespace";
+          index += text.length;
           // result
-          break
+          break;
         }
         if ((next = csp.slice(index).match(RE_SEMICOLON))) {
-          const text = next[0]
-          state = 'top'
-          index += text.length
-          break
+          const text = next[0];
+          state = "top";
+          index += text.length;
+          break;
         }
         errors.push({
           message: `parsing error`,
           index,
           state,
           text: csp.slice(index, index + 15),
-        })
-        break outer
+        });
+        break outer;
       default:
-        throw new Error('invalid state')
+        throw new Error("invalid state");
     }
   }
   return {
     result,
     warnings,
     errors,
-  }
-}
+  };
+};
